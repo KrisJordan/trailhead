ec31cb18167ea915ed1ffe09ed16a830bf6e9851
Kris Jordan <krisjordan@gmail.com>
2024-01-02 23:14:42 -0500

    Update demo files

---
+++ demo/count.py
@@ -0,0 +1,4 @@
+i = 0
+while i < 1009:
+    print("Hello, " + str(i))
+    i += 1
--- demo/hello_world.py
+++ demo/hello_world.py
@@ -1,3 +1,3 @@
-print("Hello, world!")
+print("Hello, world!!")
 name: str = input("What is your name?")
 print(f"Welcome to Trailhead, {name}!")
