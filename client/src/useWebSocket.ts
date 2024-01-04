411032af6ce8272a9626fabe6524a61e01ef8ccf
Kris Jordan <kris@cs.unc.edu>
2024-01-03 23:51:21 -0500

    Use wss connection if https; closes #7

--- client/src/useWebSocket.ts
+++ client/src/useWebSocket.ts
@@ -1,7 +1,8 @@
 import * as reactUseWebSocket from 'react-use-websocket';
 
 const HOST = window.location.host;
-const WS_ENDPOINT = `ws://${HOST}/ws`;
+const PROTOCOL = window.location.protocol === 'http:' ? 'ws:' : 'wss:';
+const WS_ENDPOINT = `${PROTOCOL}//${HOST}/ws`;
 
 export default function useWebSocket() {
     return reactUseWebSocket.default(WS_ENDPOINT, { share: true, });
