7209995d301d3716a0a82350f872f9501eaaf902
Kris Jordan <krisjordan@gmail.com>
2024-01-04 09:38:43 -0500

    Run module websocket server endpoint established

--- server/src/trailhead/models.py
+++ server/src/trailhead/models.py
@@ -4,30 +4,28 @@ from pydantic import BaseModel, Discriminator, Tag
 
 
 class Module(BaseModel):
-    ns_type: Literal['module'] = 'module'
+    ns_type: Literal["module"] = "module"
     name: str
     full_path: str
 
 
 def get_discriminator_value(v: Any) -> str | None:
     if isinstance(v, dict):
-        return v.get('ns_type') # type: ignore
-    return getattr(v, 'ns_type')
+        return v.get("ns_type")  # type: ignore
+    return getattr(v, "ns_type")
 
 
 class NamespaceTree(BaseModel):
-    ns_type: str = 'tree'
+    ns_type: str = "tree"
     children: list[
-            Annotated[
-                Union[
-                    Annotated[Module, Tag('module')],
-                    Annotated[Package, Tag('package')]
-                ],
-                Discriminator(get_discriminator_value)
-            ]
+        Annotated[
+            Union[Annotated[Module, Tag("module")], Annotated[Package, Tag("package")]],
+            Discriminator(get_discriminator_value),
         ]
+    ]
+
 
 class Package(NamespaceTree):
-    ns_type: str = 'package'
+    ns_type: str = "package"
     name: str
     full_path: str
