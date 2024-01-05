4c6b4f960621af670c0e8e460ceb0a1cceedbf77
Kris Jordan <krisjordan@gmail.com>
2024-01-04 22:12:48 -0500

    Add WebSocket Connection State to NavBar

--- client/src/home/Home.tsx
+++ client/src/home/Home.tsx
@@ -1,7 +1,9 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { NavLink, Outlet, useNavigate, useParams } from "react-router-dom";
+import { SocketState } from "../features/socket";
+import { RootState } from "../app/store";
 
 export interface HomeContext {
     navigateToModule: (module: string) => void;
@@ -12,6 +14,8 @@ export function Home() {
     console.log("params", params);
     const module = params.moduleName || null;
     const dispatch = useDispatch();
+    const { readyState } = useSelector<RootState, SocketState>((state) => state.socket);
+    console.log(readyState);
 
     // const [module, setModule] = useState<string | null>(null);
     const navigate = useNavigate();
@@ -34,6 +38,26 @@ export function Home() {
         moduleJsx = <li>{module}</li>;
     }
 
+    let indicator = null;
+    switch (readyState) {
+        case WebSocket.CONNECTING:
+            indicator = <div className="tooltip tooltip-bottom tooltip-primary mr-4" data-tip="Connecting">
+                <div className="badge badge-lg badge-accent mt-2"></div>
+            </div>;
+            break;
+        case WebSocket.OPEN:
+            indicator = <div className="tooltip tooltip-bottom tooltip-primary mr-4" data-tip="Connected">
+                <div className="badge badge-lg badge-primary mt-2"></div>
+            </div>;
+            break;
+        case WebSocket.CLOSING:
+        case WebSocket.CLOSED:
+            indicator = <div className="tooltip tooltip-bottom tooltip-error text-white mr-4" data-tip="Disconnected">
+                <div className="badge badge-lg badge-error mt-2"></div>
+            </div>;
+            break;
+    }
+
     return <>
         <div className="navbar bg-neutral text-neutral-content rounded-box mb-2">
             <div className="breadcrumbs flex-1 ">
@@ -44,7 +68,7 @@ export function Home() {
                 <div className="divider lg:divider-horizontal divider-secondary" />
                 <div className="m-0 p-0 invisible lg:visible">The Adventure Starts Here</div>
             </div>
-            <div className="flex-none"><div className="tooltip tooltip-bottom tooltip-primary mr-4" data-tip="Connected"><div className="badge badge-lg badge-primary mt-2"></div></div></div>
+            <div className="flex-none">{indicator}</div>
         </div>
         <Outlet context={{ navigateToModule }}></Outlet>
     </>;
