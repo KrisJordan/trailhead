6c616fa1607f853d703cd15902166baab5ebff48
noah <90880131+noahsmiths@users.noreply.github.com>
2024-03-25 16:38:10 -0400

    Change ModuleContext to hide GUI if not declared

--- client/src/ModuleContext.tsx
+++ client/src/ModuleContext.tsx
@@ -1,14 +1,20 @@
+import { useSelector } from "react-redux";
 import { NavLink, Outlet, useLoaderData } from "react-router-dom";
+import { RootState } from "./app/store";
+import { ModuleInfo } from "./features/module";
 
 const activeTabClass = 'tab tab-active bg-secondary font-bold text-white rounded-box';
 
 export function ModuleContext() {
+    const moduleInfo = useSelector<RootState, ModuleInfo | null>((state) => state.module.info);
     const moduleName = useLoaderData();
     return <div>
         <div role="tablist" className="tabs tabs-lg mb-2">
             <NavLink to="./run" role="tab" className={({ isActive }) => isActive ? activeTabClass : 'tab'}>Run</NavLink>
             <NavLink to="./repl" role="tab" className={({ isActive }) => isActive ? activeTabClass : 'tab'} aria-label="Interact">Interact</NavLink>
-            <NavLink to="./gui" role="tab" className={({ isActive }) => isActive ? activeTabClass : 'tab'}>GUI</NavLink>
+            {moduleInfo?.global_vars?.["__template__"] && (
+                <NavLink to="./gui" role="tab" className={({ isActive }) => isActive ? activeTabClass : 'tab'}>GUI</NavLink>
+            )}
         </div>
         <div role="tabpanel" className="bg-base-100 border-base-300 rounded-box p-6">
             <Outlet context={{ module: moduleName }} />
