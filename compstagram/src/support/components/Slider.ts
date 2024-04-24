f7346f623870897f4e1d9ef60d9eeff832b7de7b
Kris Jordan <krisjordan@gmail.com>
2024-04-23 21:38:46 -0400

    Throttle slider

--- compstagram/src/support/components/Slider.ts
+++ compstagram/src/support/components/Slider.ts
@@ -13,6 +13,8 @@ export class Slider implements Observable<number> {
         this.parent = parent;
         this.initShapes();
         this.initEventHandlers();
+        this.emit = this.emit.bind(this);
+        this.emit = this.throttle(this.emit, 250);
     }
 
     addObserver(o: Observer<number>): void {
@@ -92,6 +94,20 @@ export class Slider implements Observable<number> {
 
     private emit(): void {
         this._observers.forEach((o: Observer<number>) => o(this._value));
+    }
+
+    private throttle(mainFunction: (...args: any[]) => any, delay: number) {
+        let timerFlag: NodeJS.Timeout | null = null; // Variable to keep track of the timer
+
+        // Returning a throttled version 
+        return (...args: any[]) => {
+            if (timerFlag === null) { // If there is no timer currently running
+                mainFunction(...args); // Execute the main function 
+                timerFlag = setTimeout(() => { // Set a timer to clear the timerFlag after the specified delay
+                    timerFlag = null; // Clear the timerFlag to allow the main function to be executed again
+                }, delay);
+            }
+        };
     }
 
 }
